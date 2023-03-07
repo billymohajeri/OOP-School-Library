f026b2a9ca96af25958842a2c720891335223312
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-03-07 16:42:31 +0200

    Add nameable class

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require "nameable"
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :age, :name
 
@@ -13,9 +15,14 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
+  
 end
