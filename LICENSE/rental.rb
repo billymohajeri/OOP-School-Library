0dd2fd0fc62310659acf62eecd5762f469a14028
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-03-08 15:51:41 +0200

    Add rental class

---
+++ LICENSE/rental.rb
@@ -0,0 +1,13 @@
+class Rental
+  attr_accessor :date, :person, :book
+
+  def initialize(date, person, book)
+    @date = date
+
+    @person = person
+    person.rental << self
+
+    @book = book
+    book.rental << self
+  end
+end
