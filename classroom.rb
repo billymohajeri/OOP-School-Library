ffc3a2e3dd0777fd34097959777eda83797bd91e
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-03-16 00:48:57 +0200

    Fix linetr error

--- classroom.rb
+++ classroom.rb
@@ -1,19 +1,3 @@
-# class Classroom
-#   attr_accessor :label
-#   attr_reader :students
-
-#   def initialize(label)
-#     @label = label
-#     @students = []
-#   end
-
-#   def add_student(student)
-#     @students.push(student)
-#     student.classroom = self
-#   end
-# end
-
-
 class Classroom
   attr_accessor :label
   attr_reader :students
